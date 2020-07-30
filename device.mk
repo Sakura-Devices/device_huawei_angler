1ce18f266c36cc6b335ed8b3a2a3c184f55d524b
bgcngm <bgcngm@gmail.com>
2020-07-30 09:15:40 +0530

    Use MIUI Offline Charging Animation
    Reference- https://review.lineageos.org/#/c/205385/

--- device.mk
+++ device.mk
@@ -633,3 +633,26 @@ PRODUCT_PACKAGES += \
     RemovePackages-1 \
     RemovePackages-2 \
     RemovePackages-3
+
+# Charger
+PRODUCT_COPY_FILES += \
+    device/huawei/angler/charger/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
+    device/huawei/angler/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    device/huawei/angler/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    device/huawei/angler/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    device/huawei/angler/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    device/huawei/angler/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    device/huawei/angler/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    device/huawei/angler/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    device/huawei/angler/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    device/huawei/angler/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    device/huawei/angler/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    device/huawei/angler/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    device/huawei/angler/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    device/huawei/angler/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    device/huawei/angler/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    device/huawei/angler/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    device/huawei/angler/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    device/huawei/angler/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    device/huawei/angler/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    device/huawei/angler/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png
